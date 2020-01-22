4c9d314ef8c970d7152b2dab520cb719a9204cbb
53cur3M3 <paul_beckett@outlook.com>
2020-01-22 23:16:11 +0000

    Update videolaunch.ps1

--- videolaunch.ps1
+++ videolaunch.ps1
@@ -1,7 +1,14 @@
-$urls=@("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://bbc.co.uk/news")
+# Rick Astley : https://www.youtube.com/watch?v=dQw4w9WgXcQ
+# Weird Al Yankovic : Amish Paradise : https://www.youtube.com/watch?v=lOfZLb33uCg
+# Ylvis : What does the fox say : https://www.youtube.com/watch?v=jofNR_WkoCE
+# Katrina & The Waves : Walking on Sunshine : https://www.youtube.com/watch?v=iPUmE-tne5U
+# Weird Al Yankovic : Word Crimes : https://www.youtube.com/watch?v=8Gv0H-vPoDc
+$urls=@("https://www.youtube.com/watch?v=lOfZLb33uCg", "https://www.youtube.com/watch?v=jofNR_WkoCE", "https://www.youtube.com/watch?v=iPUmE-tne5U", "https://www.youtube.com/watch?v=8Gv0H-vPoDc")
+
+Start-Sleep -Seconds 60
 
 ForEach ($url in $urls) { 
   Start-Process $url
 
-  Start-Sleep -Milliseconds 5000
+  Start-Sleep -Seconds 60
 }
