913ec07f2cb437be2efcc123a99c5d06c618a9ad
53cur3M3 <paul_beckett@outlook.com>
2020-01-23 09:03:29 +0000

    Update pl.ps1

--- pl.ps1
+++ pl.ps1
@@ -5,10 +5,10 @@
 # Weird Al Yankovic : Word Crimes : https://www.youtube.com/watch?v=8Gv0H-vPoDc
 $urls=@("https://www.youtube.com/watch?v=lOfZLb33uCg", "https://www.youtube.com/watch?v=jofNR_WkoCE", "https://www.youtube.com/watch?v=iPUmE-tne5U", "https://www.youtube.com/watch?v=8Gv0H-vPoDc")
 
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 60
 
 ForEach ($url in $urls) { 
   Start-Process $url -WindowStyle Maximized
 
-  Start-Sleep -Seconds 10
+  Start-Sleep -Seconds 300
 }
