11c8d328d930fc4c4e9535c2a0fc7f51991d8296
53cur3M3 <paul_beckett@outlook.com>
2020-01-23 08:49:21 +0000

    Create pl.ps1

---
+++ pl.ps1
@@ -0,0 +1,14 @@
+# Rick Astley : https://www.youtube.com/watch?v=dQw4w9WgXcQ
+# Weird Al Yankovic : Amish Paradise : https://www.youtube.com/watch?v=lOfZLb33uCg
+# Ylvis : What does the fox say : https://www.youtube.com/watch?v=jofNR_WkoCE
+# Katrina & The Waves : Walking on Sunshine : https://www.youtube.com/watch?v=iPUmE-tne5U
+# Weird Al Yankovic : Word Crimes : https://www.youtube.com/watch?v=8Gv0H-vPoDc
+$urls=@("https://www.youtube.com/watch?v=lOfZLb33uCg", "https://www.youtube.com/watch?v=jofNR_WkoCE", "https://www.youtube.com/watch?v=iPUmE-tne5U", "https://www.youtube.com/watch?v=8Gv0H-vPoDc")
+
+Start-Sleep -Seconds 10
+
+ForEach ($url in $urls) { 
+  Start-Process $url -WindowStyle Maximized
+
+  Start-Sleep -Seconds 10
+}
