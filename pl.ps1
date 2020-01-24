ef60a5be2da8d8ed0e5bcb1ae0dfd62e6ce48f0f
53cur3M3 <paul_beckett@outlook.com>
2020-01-24 11:22:13 +0000

    Update pl.ps1

--- pl.ps1
+++ pl.ps1
@@ -3,9 +3,10 @@
 # Ylvis : What does the fox say : https://www.youtube.com/watch?v=jofNR_WkoCE
 # Katrina & The Waves : Walking on Sunshine : https://www.youtube.com/watch?v=iPUmE-tne5U
 # Weird Al Yankovic : Word Crimes : https://www.youtube.com/watch?v=8Gv0H-vPoDc
-$urls=@("https://www.youtube.com/watch?v=lOfZLb33uCg", "https://www.youtube.com/watch?v=jofNR_WkoCE", "https://www.youtube.com/watch?v=iPUmE-tne5U", "https://www.youtube.com/watch?v=8Gv0H-vPoDc")
+#$urls=@("https://www.youtube.com/watch?v=lOfZLb33uCg", "https://www.youtube.com/watch?v=jofNR_WkoCE", "https://www.youtube.com/watch?v=iPUmE-tne5U", "https://www.youtube.com/watch?v=8Gv0H-vPoDc")
+$urls=@("https://www.youtube.com/watch?v=8Gv0H-vPoDc")
 
-Start-Sleep -Seconds 60
+Start-Sleep -Seconds 10
 
 ForEach ($url in $urls) { 
   Start-Process $url -WindowStyle Maximized
